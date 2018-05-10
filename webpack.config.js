09d6599a7eac2d8c7ea1a57799620f6b563a4f78
Rudyhub <670752970@qq.com>
2018-05-10 16:14:45 +0800

    'webpack'

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        pageSlide: __dirname+'/PageSlide/pageSlide.js'
+    },
+    output: {
+        filename: '[name].min.js',
+        path: __dirname + '/PageSlide'
+    }
+};
